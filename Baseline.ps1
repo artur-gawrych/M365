5fc57d6340db6106fdec05cf54b2bb4a6763e106
Artur Gawrych <arthur.gawrych@gmail.com>
2022-10-20 16:40:00 +0100

    initial commit

--- Baseline.ps1
+++ Baseline.ps1
@@ -1,4 +1,5 @@
 
+Enable-OrganizationCustomization
 
 #region Enable mailbox auditing
 
@@ -48,7 +49,7 @@ if ((Get-RemoteDomain).AutoForwardEnabled -eq $True) {
 #region Configure a spam filter policy
 
 $SpamPolicyParameters = @{
-    Name                             = '[Custom] SPAM Filter Policy'
+    Name                             = '[Custom] Anti SPAM Filter Policy'
     BulkQuarantineTag                = 'DefaultFullAccessPolicy'
     BulkSpamAction                   = 'Quarantine'
     BulkThreshold                    = 7
@@ -77,9 +78,9 @@ foreach ($domain in Get-AcceptedDomain) {
 }
 
 $SpamRuleParameters = @{
-    Name                        = '[Custom] SPAM Filter Rule'
+    Name                        = '[Custom] Anti SPAM Filter Rule'
     Enabled                     = $True
-    HostedContentFilterPolicy   = '[Custom] SPAM Filter Policy'
+    HostedContentFilterPolicy   = '[Custom] Anti SPAM Filter Policy'
     Priority                    = 0
     RecipientDomainIs           = $domains
 }
@@ -91,15 +92,80 @@ New-HostedContentFilterRule @SpamRuleParameters
 #region Configure a malware filter policy
 
 $MalwarePolicyParameters = @{
-    Name                = '[Custom] Malware Filter Policy'
+    Name                = '[Custom] Anti Malware Filter Policy'
     EnableFileFilter    = $True
     FileTypes           = 'ace', 'ani', 'apk', 'app', 'appx', 'arj', 'bat', 'cmd', 'com', 'deb', 'dex', 'dll', 'docm', 'elf', 'exe', 'hta', 'img', 'jar', 'kext', 'lha', 'lib', 'library', 'lnk', 'lzh', 'macho', 'msc', 'msi', 'msix', 'msp', 'mst', 'pif', 'ppa', 'ppam', 'reg', 'rev', 'scf', 'scr', 'sct', 'sys', 'uif', 'vb', 'vbe', 'vbs', 'vxd', 'wsc', 'wsf', 'wsh', 'xll', 'xz', 'z'
     FileTypeAction      = 'Quarantine'
-    QuarantineTag       = 'AdminOnlyAccessPolicy' 
-[-RecommendedPolicyType <RecommendedPolicyType>]
-[-WhatIf]
-[-ZapEnabled <Boolean>]
-[<CommonParameters>]
+    QuarantineTag       = 'AdminOnlyAccessPolicy'
+    ZapEnabled          = $True
+}
+
+New-MalwareFilterPolicy @MalwarePolicyParameters
+
+$MalwareRuleParameters = @{
+    Name                = '[Custom] Anti Malware Filter Rule'
+    MalwareFilterPolicy = '[Custom] Anti Malware Filter Policy' 
+    Enabled             = $True
+    Priority            = 0
+    RecipientDomainIs   = $domains
+}
+
+New-MalwareFilterRule @MalwareRuleParameters
+
+#endregion
+
+#region Configure a phishing filter policy
+
+$AntiPhishPolicyParameters = @{
+   Name                                 = '[Custom] Anti Phishing Filter Policy'
+   AuthenticationFailAction             = 'MoveToJmf'
+   EnableMailboxIntelligence            = $True
+   EnableMailboxIntelligenceProtection  = $True
+   EnableOrganizationDomainsProtection  = $True
+   EnableSimilarDomainsSafetyTips       = $True
+   EnableSimilarUsersSafetyTips         = $True
+   EnableSpoofIntelligence              = $True
+   #EnableTargetedUserProtection        = $True
+   EnableUnauthenticatedSender          = $True
+   EnableUnusualCharactersSafetyTips    = $True
+   EnableViaTag                         = $True
+   ImpersonationProtectionState         = 'Automatic'
+   MailboxIntelligenceProtectionAction  = 'Quarantine'
+   MailboxIntelligenceQuarantineTag     = 'AdminOnlyAccessPolicy'
+   PhishThresholdLevel                  = 1
+   SpoofQuarantineTag                   = 'AdminOnlyAccessPolicy'
+   TargetedUserProtectionAction         = 'Quarantine'
+   TargetedUserQuarantineTag            = 'AdminOnlyAccessPolicy'
+   #TargetedUsersToProtect               = 
 
 }
 
+New-AntiPhishPolicy @AntiPhishPolicyParameters
+
+$AntiPhishingRuleParameters =@{
+    Name                = '[Custom] Anti Phishing Filter Rule'
+    AntiPhishPolicy     = '[Custom] Anti Phishing Filter Policy'
+    Enabled             = $True
+    Priority            = 0
+    RecipientDomainIs   = $domains
+}
+
+New-AntiPhishRule @AntiPhishingRuleParameters
+
+#endregion
+
+#region Enable external email tag
+
+if ((Get-ExternalInOutlook).Enabled -eq $False){
+    Set-ExternalInOutlook -Enabled $True
+    Write-Output 'Enabled "External" tag in Outlook client for emails originating from outside of the organization.'
+}
+
+#endregion
+
+#region Extend deleted item retention period.
+
+Get-Mailbox | Set-Mailbox -RetainDeletedItemsFor "30.00:00:00"
+Get-MailboxPlan | Set-MailboxPlan -RetainDeletedItemsFor "30.00:00:00"
+
+#endregion
