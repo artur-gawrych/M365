cea3814e07d1b6f1526ed1020494b2660ba7b72d
Artur Gawrych <arthur.gawrych@gmail.com>
2022-11-03 09:15:38 +0000

    changed write output

--- ADCONNECT/Change-UPN.ps1
+++ ADCONNECT/Change-UPN.ps1
@@ -8,20 +8,20 @@ param (
 $UPNSuffixList = Get-ADForest | Select-Object UPNSuffixes -ExpandProperty UPNSuffixes
 
 if ($($UPNSuffixList.Length) -ne 0) {
-    Write-Host "Checking if $NewSuffix suffix exists in AD Forest"
+    Write-Output "Checking if $NewSuffix suffix exists in AD Forest"
     foreach ($Suffix in $UPNSuffixList) {
         if ($Suffix -eq $NewSuffix) {
-            Write-Host "$NewSuffix suffix found!"
+            Write-Output "$NewSuffix suffix found!"
         } else {
-            Write-Host "$NewSuffix suffix not found in AD Forest. Attempting to add it."
+            Write-Output "$NewSuffix suffix not found in AD Forest. Attempting to add it."
             Get-ADForest | Set-ADForest -UPNSuffixes @{add = $NewSuffix }
-            Write-Host "$NewSuffix suffix was added successfully"
+            Write-Output "$NewSuffix suffix was added successfully"
         }
     }
 } else {
-    Write-Host "$NewSuffix suffix not found in AD Forest. Attempting to add it."
+    Write-Output "$NewSuffix suffix not found in AD Forest. Attempting to add it."
     Get-ADForest | Set-ADForest -UPNSuffixes @{add = $NewSuffix }
-    Write-Host "$NewSuffix suffix was added successfully"
+    Write-Output "$NewSuffix suffix was added successfully"
 }
 
 # Change the UPNsuffix for all users.
@@ -29,6 +29,6 @@ if ($($UPNSuffixList.Length) -ne 0) {
 $users = Get-ADUser -Filter * -Properties *
 
 foreach ($user in $users) {
-    Write-Host "Changing UPN for $($user.Name) - $($user.SamAccountName)"
+    Write-Output "Changing UPN for $($user.Name) - $($user.SamAccountName)"
     Set-ADUser -Identity $user.SamAccountName -UserPrincipalName $($user.SamAccountName + "@" + $NewSuffix)
 }
